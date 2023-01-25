1ea2076943d250cedca6b05648d21d0bf5cef1c5
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2023-01-25 10:00:45 +0100

    Create so_long.h

---
+++ sources/so_long.h
@@ -0,0 +1,28 @@
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# define (pictures) "location pictures"
+
+# define (keys) "numbe"
+
+# include <unistd.h>
+# include <mlx.h>
+
+typedef struct s_info_map
+{
+	int	hight;
+	int	weight;
+	int	x;
+	int	y;
+	int	player;
+	int	count;
+	int	finish;
+	int	step;			//ver para que sirve
+	char	**map;
+}	t_info_map;
+
+	/---	ft_check_map	---/
+void	ft_check_outline(t_info_map *data)
+void	ft_check_format_dotber(t_info_map *data)
+void	ft_check_map_inputs(t_info_map *data)
+void	ft_check_map_objects(t_info_map *data)

084a6a22699764a83f53e1dc608e190fce0f69b7
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2023-02-01 10:36:35 +0100

    Update ft_create_map.c

--- sources/ft_create_map.c
+++ sources/ft_create_map.c
@@ -6,3 +6,26 @@ int	ft_exit(t_info_map *data)
 	exit(EXIT_SUCCESS);
 }
 
+void	put_player_img()
+
+void	ft_view_map(t_info_map *data)
+{
+	int	i;
+	int	j;
+	
+	i = 0;
+	j = 0;
+	while (data->map[i][j])
+	{
+		if (data->map[i][j] == '1')
+			put_wall_img(data, &i, &j);
+		else if (data->map[i][j] == '0')
+			put_empty_img(data, &i, &j);
+		else if (data->map[i][j] == 'P')
+			put_player_img(data, &i, &j);
+		else if (data->map[i][j] == 'C')
+			put_empty_img(data, &i, &j);
+		else if (data->map[i][j] == 'E')
+			put_empty_img(data, &i, &j);
+	}
+}

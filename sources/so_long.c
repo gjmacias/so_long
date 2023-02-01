d1beafbb244af14665ce1b44d0731fba7e5ccb1a
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2023-02-01 14:41:38 +0100

    Update so_long.c

--- sources/so_long.c
+++ sources/so_long.c
@@ -1,14 +1,51 @@
 #include "so_long.h"
 
+void	ft_load_img(t_info_map *data)
+{
+	int	img_hight;
+	int	img_width;
+	
+	data->graph = malloc(sizeof(t_graph));
+	data->graph->player = mlx_xpm_file_to_image(data->mlx
+			PLAYER, &img_width, &img_hight);
+	data->graph->wall = mlx_xpm_file_to_image(data->mlx
+			WALL, &img_width, &img_hight);
+	data->graph->empty = mlx_xpm_file_to_image(data->mlx
+			EMPTY, &img_width, &img_hight);
+	data->graph->exit = mlx_xpm_file_to_image(data->mlx
+			EXIT, &img_width, &img_hight);
+	data->graph->collect = mlx_xpm_file_to_image(data->mlx
+			COLLECT, &img_width, &img_hight);
+	data->graph->win = mlx_xpm_file_to_image(data->mlx
+			WIN, &img_width, &img_hight);
+}
+
 void	ft_reset_data(t_info_map *data, char *name)
 {
 	data->hight = 0;
-	data->weight = 0;
+	data->width = 0;
 	data->x = 0;
 	data->y = 0;
 	data->player = 0;
-	data->step = 0;		//ver para que sirve
+	data->step = 0;
 	data->txt = name;
+	ft_load_img(data);
+}
+
+void	ft_general_check(t_info_map *data)
+{
+	ft_check_outline(data);
+	ft_check_format_dotber(data);
+	ft_check_map_inputs(data);
+	ft_check_map_objects(data);
+}
+
+int	ft_frame(t_info_map *data)
+{
+	mlx_clear_windows(data->mlx, data->win);
+	ft_draw_map(data);
+	if(data->count == 0 && data->player == 1 && data->finish == 1)
+		ft_game_result(data);
 }
 
 int	main(int argc, char **argv)
@@ -19,6 +56,14 @@ int	main(int argc, char **argv)
 	{
 		data.mlx = mlx_init();
 		ft_reset_data(&data, argv[1]);
-		
+		ft_map_size(&data);
+		ft_malloc_map(&data);
+		ft_general_check(&data);
+		data.win = mlx_new_window(data.mlx, data.width * 40,
+					  data.hight * 40, "so_long");
+		mlx_hook(data.win, 17, 0, ft_exit, &data);
+		mlx_hook(data.win, 02, 0, ft_press_key, &data);
+		mlx_loop_hook(data.mlx, ft_frame, &data);
+		mlx_loop(data.mlx);
 	}
 }
